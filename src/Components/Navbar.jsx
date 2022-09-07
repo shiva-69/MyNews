064e5d8289c01a95c5216da8bf4eaf37c66f81c6
Shivam Pandey <shivam8kp@gmail.com>
2022-09-07 13:48:10 +0530

    added navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,20 @@
-import { Box, Flex} from "@chakra-ui/react";
+import { Box, Flex, Heading, Spacer, } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
 
 export const Navbar = () => {
     return (
-        <Flex direction="row">
-            <Box>NEWS</Box>
-            <Box><Link to ="/">Home</Link></Box>
-            <Box><Link to ="/sports">Sports</Link></Box>
-            <Box><Link to ="/travel">Travel</Link></Box>
-        </Flex>
-
+        <Flex minWidth='max-content' alignItems='center' gap='1' pl="6%" pr="6%" bg="black" color="white">
+        <Box p='2'>
+          <Heading size='lg'>NEWS</Heading>
+        </Box>
+        <Spacer/>
+          <Flex gap="10" mr="10%" fontWeight="600">
+            <Box ><Link to="/">Home</Link></Box>
+            <Box ><Link to="/">Sports</Link></Box>
+            <Box ><Link to="/">Travel</Link></Box>
+          </Flex >
+      </Flex>
     )
 }
